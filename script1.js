f3b952afa82f032a9f3e269f5a6c2d08d3130a5b
nehaldangi <95690030+nehaldangi@users.noreply.github.com>
2023-06-10 22:07:53 +0400

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-const navEl = document.querySelector('.navbar');
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 56) {
-        navEl.classList.add('navbar-scrolled')
-    } else if (window.scrollY < 56) {
-        navEl.classList.remove('navbar-scrolled')
-    }
-});
